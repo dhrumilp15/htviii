438f7b518e496364835fa88746115114923811f5
Dhrumil Patel <dhrumilp15@gmail.com>
2019-02-23 10:18:03 -0500

    Got the Search button to appear

--- app/app/src/main/java/com/onionhasmayo/htv_iii/MainActivity.java
+++ app/app/src/main/java/com/onionhasmayo/htv_iii/MainActivity.java
@@ -1,5 +1,6 @@
 package com.onionhasmayo.htv_iii;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,14 +9,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE =  "com.onionhasmayo.htv_iii.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar  toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
